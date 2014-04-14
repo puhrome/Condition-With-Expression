92e2e6c63cd33cc38b79c4b7954b25824b0a9ad3
puhrome <phirom.yim@fullsail.edu>
2014-04-14 12:52:36 -0400

    Condition With Expression
    Use WebStorm to add expression to relational statement. Adding value
    and or variables to change outcome of statement.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Conditional With Expression Created by Phirom Yim on 4/14/14.
+ */
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is tall enough, print to the console, you can ride
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight) {
+    //code performed if condition is true
+    //condition goes inside of ( )
+    //code block goes inside of { }
+    console.log("You can ride the coaster!");
+}
